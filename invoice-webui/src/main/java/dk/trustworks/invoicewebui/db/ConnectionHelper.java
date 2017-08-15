1c5e3b5f601fd137113c841291f50a4eada15504
hans <hans@godfather.dk>
2017-08-15 18:45:50 +0200

    Changed to env values

--- invoice-webui/src/main/java/dk/trustworks/invoicewebui/db/ConnectionHelper.java
+++ invoice-webui/src/main/java/dk/trustworks/invoicewebui/db/ConnectionHelper.java
@@ -3,7 +3,7 @@ package dk.trustworks.invoicewebui.db;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-import java.util.Properties;
+import java.util.Map;
 
 /**
  * Created by hans on 15/12/2016.
@@ -17,16 +17,23 @@ public class ConnectionHelper {
     private ConnectionHelper() {
         //jdbc:mysql://trustworksdb.cm3iylt6ulsl.eu-west-1.rds.amazonaws.com:3306/
 
-        Properties props = System.getProperties();
+        //Properties props = System.getProperties();
+        Map<String, String> env = System.getenv();
+
         //props.setProperty("dataSourceClassName", "org.postgresql.ds.PGSimpleDataSource");
         //props.setProperty("dataSource.user", "test");
         //props.setProperty("dataSource.password", "test");
         //props.setProperty("dataSource.databaseName", "mydb");
 
         HikariConfig config = new HikariConfig();
+        /*
         config.setJdbcUrl(props.getProperty("db.url"));
         config.setUsername(props.getProperty("db.username"));
         config.setPassword(props.getProperty("db.password"));
+        */
+        config.setJdbcUrl(env.get("db.url"));
+        config.setUsername(env.get("db.username"));
+        config.setPassword(env.get("db.password"));
         config.addDataSourceProperty("cachePrepStmts", "true");
         config.addDataSourceProperty("prepStmtCacheSize", "250");
         config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
