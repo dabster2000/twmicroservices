e971e73befc8bfd272b10cda3a8216f49ae30093
dabster2000 <160675am>
2018-04-03 15:22:18 +0200

    Better password protection!

--- invoice-webui/src/main/java/dk/trustworks/invoicewebui/web/login/components/ResetPasswordImpl.java
+++ invoice-webui/src/main/java/dk/trustworks/invoicewebui/web/login/components/ResetPasswordImpl.java
@@ -7,6 +7,7 @@ import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Notification;
 import dk.trustworks.invoicewebui.model.User;
 import dk.trustworks.invoicewebui.repositories.UserRepository;
+import dk.trustworks.invoicewebui.security.PasswordConstraintValidator;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -27,7 +28,7 @@ public class ResetPasswordImpl extends ResetPasswordDesign {
         System.out.println("LoginImpl.LoginImpl");
         getImgTop().setSource(new ThemeResource("images/password-card.jpg"));
         getBtnSave().addClickListener(clickEvent -> {
-            if(getTxtPassword().getValue().equals(getTxtVerifyPassword().getValue())) {
+            if (getTxtPassword().getValue().equals(getTxtVerifyPassword().getValue())) {
                 User userDb = userRepository.findOne(user.getUuid());
                 userDb.setPassword(BCrypt.hashpw(getTxtPassword().getValue(), BCrypt.gensalt()));
                 userRepository.save(userDb);
@@ -35,6 +36,14 @@ public class ResetPasswordImpl extends ResetPasswordDesign {
                 Notification.show("Succes",
                         "Your may now use your new password.",
                         Notification.Type.ASSISTIVE_NOTIFICATION);
+            } else if (new PasswordConstraintValidator().isValid(getTxtPassword().getValue())) {
+                Notification.show("Failed",
+                        "Password not strong enough!",
+                        Notification.Type.WARNING_MESSAGE);
+                getTxtPassword().setValue("");
+                getTxtPassword().setWidth(100, Unit.PERCENTAGE);
+                getTxtVerifyPassword().setValue("");
+                getTxtVerifyPassword().setWidth(100, Unit.PERCENTAGE);
             } else {
                 Notification.show("Failed",
                         "The passwords do not match, please try again.",
