2d46ac2b6f7e08f7f7f9924ca3b8648d1f8a63e5
dabster2000 <160675am>
2018-04-04 11:01:11 +0200

    Fixed password validator

--- invoice-webui/src/main/java/dk/trustworks/invoicewebui/security/PasswordConstraintValidator.java
+++ invoice-webui/src/main/java/dk/trustworks/invoicewebui/security/PasswordConstraintValidator.java
@@ -8,15 +8,7 @@ import java.util.Arrays;
 public class PasswordConstraintValidator {
 
     public boolean isValid(String password) {
-        PasswordValidator validator = new PasswordValidator(Arrays.asList(
-                new LengthRule(8, 30),
-                new UppercaseCharacterRule(1),
-                new DigitCharacterRule(1),
-                new SpecialCharacterRule(1),
-                new NumericalSequenceRule(3,false),
-                new AlphabeticalSequenceRule(3,false),
-                new QwertySequenceRule(3,false),
-                new WhitespaceRule()));
+        PasswordValidator validator = new PasswordValidator(Arrays.asList(new LengthRule(8, 30)));
 
         RuleResult result = validator.validate(new PasswordData(password));
         if (result.isValid()) {
