5ea9424edbce8f8395af4629dbf05ce072a1fe8c
dabster2000 <160675am>
2018-10-01 11:59:53 +0200

    Fixed property

--- invoice-webui/src/main/java/dk/trustworks/invoicewebui/repositories/CertificationRepository.java
+++ invoice-webui/src/main/java/dk/trustworks/invoicewebui/repositories/CertificationRepository.java
@@ -4,7 +4,6 @@ package dk.trustworks.invoicewebui.repositories;
  * Created by hans on 27/06/2017.
  */
 
-import dk.trustworks.invoicewebui.model.Ambition;
 import dk.trustworks.invoicewebui.model.Certification;
 import dk.trustworks.invoicewebui.model.User;
 import org.springframework.data.repository.CrudRepository;
@@ -14,7 +13,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "certifications", path="certifications")
-public interface CertificationRepository extends CrudRepository<Ambition, Integer> {
+public interface CertificationRepository extends CrudRepository<Certification, Integer> {
 
     List<Certification> findByUser(@Param("user") User user);
 
