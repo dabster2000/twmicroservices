1271705bf725178858c34107293a483a1533ff4d
dabster2000 <160675am>
2019-03-21 12:04:23 +0100

    Fix: admin may now upload new images to bubbles

--- invoice-webui/src/main/java/dk/trustworks/invoicewebui/web/bubbles/BubblesLayout.java
+++ invoice-webui/src/main/java/dk/trustworks/invoicewebui/web/bubbles/BubblesLayout.java
@@ -175,7 +175,7 @@ public class BubblesLayout extends VerticalLayout {
             Resource resource = photoService.getRelatedPhoto(relatedUUID);
 
             bubblesDesign.getImgTop().setSource(resource);
-            if(bubble.getUser().getUuid().equals(user.getUuid()) || bubble.getUser().getUsername().equals("hans.lassen")) {
+            if(bubble.getUser().getUuid().equals(user.getUuid()) || user.getUsername().equals("hans.lassen")) {
                 bubblesDesign.getImgTop().addClickListener(event -> bubbleForm.editPhotoAction(bubble));
             }
 
