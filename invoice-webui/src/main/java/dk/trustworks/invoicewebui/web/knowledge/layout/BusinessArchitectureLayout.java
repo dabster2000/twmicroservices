e9af5ebd93afbb06fa4c7897bb935846a306e8dc
dabster2000 <160675am>
2019-05-07 10:25:57 +0200

    Feat: Further design of business architecture site

--- invoice-webui/src/main/java/dk/trustworks/invoicewebui/web/knowledge/layout/BusinessArchitectureLayout.java
+++ invoice-webui/src/main/java/dk/trustworks/invoicewebui/web/knowledge/layout/BusinessArchitectureLayout.java
@@ -3,12 +3,15 @@ package dk.trustworks.invoicewebui.web.knowledge.layout;
 import com.jarektoro.responsivelayout.ResponsiveLayout;
 import com.jarektoro.responsivelayout.ResponsiveRow;
 import com.vaadin.server.ThemeResource;
+import com.vaadin.shared.ui.ContentMode;
 import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 import dk.trustworks.invoicewebui.web.common.Box;
 import dk.trustworks.invoicewebui.web.common.ImageCardDesign;
-import dk.trustworks.invoicewebui.web.knowledge.components.ArchitecturePerspectiveColumn;
+import dk.trustworks.invoicewebui.web.knowledge.components.SideBannerDesign;
 import org.vaadin.viritin.label.MLabel;
 import org.vaadin.viritin.layouts.MVerticalLayout;
 
@@ -25,62 +28,77 @@ public class BusinessArchitectureLayout extends VerticalLayout {
 
         mainLayout = new ResponsiveLayout(ResponsiveLayout.ContainerType.FLUID);
         Box mainBox = new Box();
+        mainBox.getCardHolder().setWidthUndefined();
+        mainBox.getContent().setWidthUndefined();
         mainBox.getContent().addComponent(mainLayout);
         mainBox.getContent().setMargin(false);
         this.addComponent(mainBox);
 
         gridRow = mainLayout.addRow();
 
-        /*
-        ArchitecturePerspectiveColumn taskColumn = createArchitectureColumn("Opgaver", "dark-blue");
-        ArchitecturePerspectiveColumn informationColumn = createArchitectureColumn("Information", "bg-secondary-1-2");
-        ArchitecturePerspectiveColumn applicationColumn = createArchitectureColumn("Applikation", "bg-secondary-2-0");
-        ArchitecturePerspectiveColumn strategyColumn = createArchitectureColumn("Strategi", "yellow");
-        ArchitecturePerspectiveColumn controlColumn = createArchitectureColumn("Styring", "turquoise");
-        */
+        VerticalLayout firstColumn = createArchitectureColumn2("-", "bg-grey", "");
+        firstColumn.addComponents(
+                createVerticalHeadline("Konceptuel", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+                createVerticalHeadline("Logisk", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+                createVerticalHeadline("Fysisk", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."));
 
-        gridRow.addColumn()
-                .withStyleName("bg-grey")
-                .withDisplayRules(12, 12, 2, 2)
-                .withComponent(
-                        new MVerticalLayout(
-                                new MLabel("Description").withFullWidth().withStyleName("large bold align-center"),
-                                new MLabel("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-                                        .withStyleName("small").withFullWidth()
-                        )
-        );
-        VerticalLayout taskColumn = createArchitectureColumn2("Opgaver", "dark-blue");
-        VerticalLayout informationColumn = createArchitectureColumn2("Information", "bg-secondary-1-2");
-        VerticalLayout applicationColumn = createArchitectureColumn2("Applikation", "bg-secondary-2-0");
-        VerticalLayout strategyColumn = createArchitectureColumn2("Strategi", "yellow");
-        VerticalLayout controlColumn = createArchitectureColumn2("Styring", "turquoise");
+        VerticalLayout taskColumn = createArchitectureColumn2("Opgaver", "blue-bg", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+        VerticalLayout informationColumn = createArchitectureColumn2("Information", "bg-secondary-1-2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+        VerticalLayout applicationColumn = createArchitectureColumn2("Applikation", "bg-secondary-2-0", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+        VerticalLayout strategyColumn = createArchitectureColumn2("Strategi", "yellow", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+        VerticalLayout controlColumn = createArchitectureColumn2("Styring", "turquoise", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
 
-        taskColumn.addComponent(createCard(1, "Forretningsstruktur"));
-        taskColumn.addComponent(createCard(2, "Processer"));
-        taskColumn.addComponent(createCard(3, "Arbejdstilrettelæggelse"));
+        createRows(taskColumn, "opgaver",
+                "Forretningsstruktur",
+                "<ul style='list-style-type:square;'><li>Opgave-/Servicekatalog</li>" +
+                "<li>Domænemodel</li>" +
+                "<li>Proceslandskab</li></ul>",
 
+                "Processer",
+                "- Aktører/roller<br/>" +
+                "- Procesmodel<br/>" +
+                "- Brugerrejse<br/>" +
+                "- Servicemodel<br/>" +
+                "- Use case / user story",
 
-        for (int i = 0; i < 3; i++) {
-            Box box = new Box();
-            box.getContent().addComponent(new MLabel("Forretningsobjekter og begreber\n"));
-            box.getContent().setHeight(150, Unit.PIXELS);
-            //informationColumn.getContent().addComponent(box);
-            informationColumn.addComponent(box);
-        }
+                "Arbejdstilrettelæggelse",
+                "- Arbejdsgang / -beskrivelse");
+
+        createRows(informationColumn, "information",
+                "Forretningsobjekter og begreber",
+                "- Centrale forretningsobjekter<br/>" +
+                "- Begrebsliste / model",
+
+                "Logiske datamodeller",
+                "- Informationsmodel<br/>" +
+                        "- Logisk datamodel<br/>" +
+                        "- Masterdata<br/>" +
+                        "- Datakvalitet<br/>",
+
+                "Fysiske datamodeller",
+                "- Datasæt<br/>" +
+                                "- Dataudvekslings-format");
+
+        createRows(applicationColumn,"applikation",
+                "Applikationsstruktur og integrationsmønstre",
+                "- Systemlandskab / kontekstdiagram",
+
+                "Applikationslandskab og integrationer",
+                "- Applikationslandskab / +integration<br/>" +
+                        "- Applikationer mappet til forretning<br/>" +
+                        "- Applikationer mappet til information",
+
+                "Applikationsdesign og konfiguration",
+                "- Applikationsdesign<br/>" +
+                        "- Løsningskomponent<br/>" +
+                        "- Snitfladebeskrivelser<br/>" +
+                        "- Testscenarier");
+
 
         for (int i = 0; i < 3; i++) {
             Box box = new Box();
             box.getContent().addComponent(new MLabel("Opgave-/Servicekatalog"));
             box.getContent().setHeight(150, Unit.PIXELS);
-            //applicationColumn.getContent().addComponent(box);
-            applicationColumn.addComponent(box);
-        }
-
-        for (int i = 0; i < 3; i++) {
-            Box box = new Box();
-            box.getContent().addComponent(new MLabel("Opgave-/Servicekatalog"));
-            box.getContent().setHeight(150, Unit.PIXELS);
-            //strategyColumn.getContent().addComponent(box);
             strategyColumn.addComponent(box);
         }
 
@@ -88,34 +106,55 @@ public class BusinessArchitectureLayout extends VerticalLayout {
             Box box = new Box();
             box.getContent().addComponent(new MLabel("Opgave-/Servicekatalog"));
             box.getContent().setHeight(150, Unit.PIXELS);
-            //controlColumn.getContent().addComponent(box);
             controlColumn.addComponent(box);
         }
 
         return this;
     }
 
-    private ImageCardDesign createCard(int i, String title) {
+    private Component createVerticalHeadline(String konceptuel, String description) {
+        SideBannerDesign card = new SideBannerDesign();
+        card.addStyleName("semi-white-bg");
+        card.getVlContent().setHeight(175, Unit.PIXELS);
+        Label label = new MLabel(konceptuel).withFullWidth().withStyleName("bold large align-center");
+        MLabel descriptionLabel = new MLabel(description).withFullSize().withStyleName("align-center tiny");
+        card.getVlContent().addComponents(label);
+        card.getVlContent().addComponentsAndExpand(descriptionLabel);
+        card.getVlContent().setSpacing(false);
+        card.getImgTop().setSource(new ThemeResource("images/cards/architecture/transparent2.png"));
+        return card;
+    }
+
+    private void createRows(VerticalLayout column, String type, String title1, String content1, String title2, String content2, String title3, String content3) {
+        column.addComponent(createCard(type, 1, title1, content1));
+        column.addComponent(createCard(type, 2, title2, content2));
+        column.addComponent(createCard(type, 3, title3, content3));
+    }
+
+    private ImageCardDesign createCard(String type, int i, String title, String content) {
         ImageCardDesign box = new ImageCardDesign();
-        box.getImgTop().setSource(new ThemeResource("images/cards/architecture/opgaver-"+i+".png"));
-        box.getVlContent().addComponent(new VerticalLayout(new MLabel(title).withFullWidth().withStyleName("small bold")));
-        box.getVlContent().setHeight(150, Unit.PIXELS);
+        box.addStyleName("semi-white-bg");
+        box.getImgTop().setSource(new ThemeResource("images/cards/architecture/"+type+"-"+i+".png"));
+        box.getVlContent().addComponent(
+                new MVerticalLayout(
+                        new MLabel(title)
+                                .withFullWidth()
+                                .withStyleName("small bold"),
+                        new MLabel(content)
+                                .withContentMode(ContentMode.HTML)
+                                .withFullWidth()
+                                .withStyleName("tiny bold")
+                )
+        );
+        box.getVlContent().setHeight(175, Unit.PIXELS);
         return box;
     }
 
-    private ArchitecturePerspectiveColumn createArchitectureColumn(String opgaver, String color) {
-        ArchitecturePerspectiveColumn architecturePerspectiveColumn = new ArchitecturePerspectiveColumn();
-        architecturePerspectiveColumn.getLblHeading().setValue(opgaver);
-        architecturePerspectiveColumn.getCardHolder().addStyleName(color);
-        gridRow.addColumn()
-                .withDisplayRules(12, 12, 2, 2)
-                .withStyleName("color")
-                .withComponent(architecturePerspectiveColumn);
-        return architecturePerspectiveColumn;
-    }
-
-    private VerticalLayout createArchitectureColumn2(String opgaver, String color) {
-        MVerticalLayout column = new MVerticalLayout(new MLabel(opgaver).withFullWidth().withStyleName("align-center large bold")).withWidth(100, Unit.PERCENTAGE);
+    private VerticalLayout createArchitectureColumn2(String opgaver, String color, String description) {
+        MVerticalLayout column = new MVerticalLayout(
+                new MLabel(opgaver).withFullWidth().withStyleName("align-center large bold")
+                //new MLabel(description).withFullWidth().withStyleName("align-center small")
+        ).withWidth(100, Unit.PERCENTAGE).withMargin(false);
 
         gridRow.addColumn()
                 .withDisplayRules(12, 12, 2, 2)
