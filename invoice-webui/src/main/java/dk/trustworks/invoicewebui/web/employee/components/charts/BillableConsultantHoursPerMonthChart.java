c8cd1ddc3530c93525fd8f7ae0b14ebef43ecd9f
dabster2000 <160675am>
2019-05-20 14:44:04 +0200

    Feat: Added hourly budget to consultant chart.

--- invoice-webui/src/main/java/dk/trustworks/invoicewebui/web/employee/components/charts/BillableConsultantHoursPerMonthChart.java
+++ invoice-webui/src/main/java/dk/trustworks/invoicewebui/web/employee/components/charts/BillableConsultantHoursPerMonthChart.java
@@ -5,18 +5,17 @@ import com.vaadin.addon.charts.model.*;
 import com.vaadin.server.Sizeable;
 import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.spring.annotation.SpringUI;
-import dk.trustworks.invoicewebui.model.GraphKeyValue;
 import dk.trustworks.invoicewebui.model.User;
-import dk.trustworks.invoicewebui.repositories.GraphKeyValueRepository;
+import dk.trustworks.invoicewebui.services.StatisticsService;
+import dk.trustworks.invoicewebui.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
+
+import static dk.trustworks.invoicewebui.utils.DateUtils.stringIt;
 
 /**
  * Created by hans on 20/09/2017.
@@ -26,11 +25,14 @@ import java.util.stream.Collectors;
 @SpringUI
 public class BillableConsultantHoursPerMonthChart {
 
-    private final GraphKeyValueRepository graphKeyValueRepository;
+    private final StatisticsService statisticsService;
+
+    private final UserService userService;
 
     @Autowired
-    public BillableConsultantHoursPerMonthChart(GraphKeyValueRepository graphKeyValueRepository) {
-        this.graphKeyValueRepository = graphKeyValueRepository;
+    public BillableConsultantHoursPerMonthChart(StatisticsService statisticsService, UserService userService) {
+        this.statisticsService = statisticsService;
+        this.userService = userService;
     }
 
     @Cacheable("revenueChart")
@@ -41,7 +43,7 @@ public class BillableConsultantHoursPerMonthChart {
         chart.setWidth(100, Sizeable.Unit.PERCENTAGE);
         int months = (int)ChronoUnit.MONTHS.between(periodStart, periodEnd);
 
-        chart.setCaption("Hours during Fiscal Year 07/"+(periodStart.getYear())+" - 06/"+periodEnd.getYear());
+        chart.setCaption("Billable and budgeted hours during fiscal year 07/"+(periodStart.getYear())+" - 06/"+periodEnd.getYear());
         chart.getConfiguration().setTitle("");
         chart.getConfiguration().getChart().setType(ChartType.AREASPLINE);
         chart.getConfiguration().getChart().setAnimation(true);
@@ -54,21 +56,19 @@ public class BillableConsultantHoursPerMonthChart {
         tooltip.setFormatter("this.series.name +': '+ Highcharts.numberFormat(this.y, 0) +' hours'");
         chart.getConfiguration().setTooltip(tooltip);
 
-        List<GraphKeyValue> amountPerItemList = graphKeyValueRepository.findBillableHoursByMonthByPeriod(user.getUuid(), periodStart.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")), periodEnd.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-
         String[] categories = new String[months];
-        DataSeries revenueSeries = new DataSeries("Hours");
-        amountPerItemList = amountPerItemList.stream().sorted(Comparator.comparing(o -> LocalDate.parse(o.getDescription(), DateTimeFormatter.ofPattern("yyyy-M-dd")))).collect(Collectors.toList());
+        DataSeries revenueSeries = new DataSeries("Billable hours");
+        DataSeries budgetSeries = new DataSeries("Budgeted hours");
         for (int i = 0; i < months; i++) {
             LocalDate currentDate = periodStart.plusMonths(i);
-            if(amountPerItemList.size() > i) {
-                GraphKeyValue amountPerItem = amountPerItemList.get(i);
-                if(amountPerItem!=null) revenueSeries.add(new DataSeriesItem(LocalDate.parse(amountPerItem.getDescription(), DateTimeFormatter.ofPattern("yyyy-M-dd")).format(DateTimeFormatter.ofPattern("MMM-yyyy")), amountPerItem.getValue()));
-            }
+            revenueSeries.add(new DataSeriesItem(stringIt(currentDate, "MMM-yyyy"), statisticsService.getConsultantRevenueHoursByMonth(user, currentDate)));
+            budgetSeries.add(new DataSeriesItem(stringIt(currentDate,"MMM-yyyy"), statisticsService.getConsultantBudgetHoursByMonth(user, currentDate)));
+
             categories[i] = currentDate.format(DateTimeFormatter.ofPattern("MMM-yyyy"));
         }
         chart.getConfiguration().getxAxis().setCategories(categories);
         chart.getConfiguration().addSeries(revenueSeries);
+        chart.getConfiguration().addSeries(budgetSeries);
         Credits c = new Credits("");
         chart.getConfiguration().setCredits(c);
         return chart;
